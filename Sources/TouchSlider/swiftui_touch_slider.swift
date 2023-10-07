a8695fc55ba8579c53690edf6191c65980e6c980
Muukii <muukii.app@gmail.com>
2023-10-08 03:39:10 +0900

    :evergreen_tree: Update

--- Sources/TouchSlider/swiftui_touch_slider.swift
+++ Sources/TouchSlider/swiftui_touch_slider.swift
@@ -121,3 +121,132 @@ public struct TouchSlider: View {
   }
 
 }
+
+extension Color {
+
+  static var vibrantBlue: Color {
+    // #0600FF
+    Color(.displayP3, red: 0.023529411764705882, green: 0.0, blue: 1.0, opacity: 1)
+  }
+
+  static var vibrantOrange: Color {
+    // #FF7A00
+    Color(.displayP3, red: 1.0, green: 0.47843137254901963, blue: 0.0, opacity: 1)
+  }
+
+  static var vibrantRed: Color {
+    // #FF3D00
+    Color(.displayP3, red: 1.0, green: 0.23921568627450981, blue: 0.0, opacity: 1)
+  }
+
+  static var vibrantPurple: Color {
+    // #8F00FF
+    Color(.displayP3, red: 0.5607843137254902, green: 0.0, blue: 1.0, opacity: 1)
+  }
+
+  static var vibrantYellow: Color {
+    // #FFA800
+    Color(.displayP3, red: 1.0, green: 0.6588235294117647, blue: 0.0, opacity: 1)
+  }
+
+  static var vibrantPink: Color {
+    // #ED0047
+    Color(.displayP3, red: 0.9294117647058824, green: 0.0, blue: 0.2784313725490196, opacity: 1)
+  }
+
+  static var vibrantGreen: Color {
+    // #00E124
+    Color(.displayP3, red: 0.0, green: 0.8823529411764706, blue: 0.1411764705882353, opacity: 1)
+  }
+
+}
+
+#if DEBUG
+struct BookSlider: View {
+
+  @State var slider_value: Double = 102
+  @State var touchSlider_value: Double = 0
+
+  var body: some View {
+
+    VStack {
+
+      Spacer()
+
+      VStack {
+        VStack {
+          TouchSlider(
+            direction: .horizontal,
+            value: $touchSlider_value,
+            speed: 1,
+            foregroundColor: Color.vibrantBlue,
+            backgroundColor: Color(white: 0.5, opacity: 0.3)
+          )
+          .frame(height: 20)
+
+          TouchSlider(
+            direction: .horizontal,
+            value: $touchSlider_value,
+            speed: 1,
+            foregroundColor: Color.vibrantPink,
+            backgroundColor: Color(white: 0.5, opacity: 0.3),
+            cornerRadius: 16
+          )
+          .frame(height: 40)
+
+          TouchSlider(
+            direction: .horizontal,
+            value: $touchSlider_value,
+            speed: 1,
+            foregroundColor: Color.vibrantRed,
+            backgroundColor: Color(white: 0.5, opacity: 0.3),
+            cornerRadius: 16
+          )
+          .frame(height: 80)
+        }
+        .frame(maxHeight: .infinity)
+
+        HStack(spacing: 20) {
+          TouchSlider(
+            direction: .vertical,
+            value: $touchSlider_value,
+            speed: 1,
+            foregroundColor: Color.vibrantPink,
+            backgroundColor: Color(white: 0.5, opacity: 0.3)
+          )
+          .frame(width: 20)
+
+          TouchSlider(
+            direction: .vertical,
+            value: $touchSlider_value,
+            speed: 1,
+            foregroundColor: Color.vibrantPurple,
+            backgroundColor: Color(white: 0.5, opacity: 0.3),
+            cornerRadius: 16
+          )
+          .frame(width: 40)
+
+          TouchSlider(
+            direction: .vertical,
+            value: $touchSlider_value,
+            speed: 1,
+            foregroundColor: Color.vibrantYellow,
+            backgroundColor: Color(white: 0.5, opacity: 0.3),
+            cornerRadius: 16
+          )
+          .frame(width: 80)
+        }
+        .frame(maxHeight: .infinity)
+      }
+      .padding(.horizontal)
+
+    }
+    .onAppear {
+    }
+  }
+}
+
+#Preview {
+  BookSlider()
+}
+#endif
